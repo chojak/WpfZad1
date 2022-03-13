4999b0e747465091c7bdcfc5aae613c937363ac0
chojak <chojecki2000@gmail.com>
2022-03-13 15:49:57 +0100

    Add project files.

---
+++ WpfZad1/LotteryMachine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfZad1
+{
+    internal class LotteryMachine
+    {
+        List<string> coupons = new List<string>();
+        public void AddNewCoupon(string coupon)
+        {
+            coupons.Add(coupon);
+        }
+        public string GetRandomCoupon()
+        {
+            Random random = new Random();
+            int couponIndex;
+
+            if (coupons.Count > 0)
+            {
+                couponIndex = random.Next(0, coupons.Count);
+                string coupon = coupons[couponIndex];
+                coupons.RemoveAt(couponIndex);
+
+                return coupon;
+            }
+
+            return "No coupon available";
+        }
+        public bool CouponsAvailable()
+        {
+            return coupons.Count > 0;
+        }
+        public string GetAllCoupons()
+        {
+            string rtn = "";
+
+            if (CouponsAvailable())
+            {
+                for (int i = 0; i < coupons.Count; i++)
+                {
+                    rtn += (i + 1) + ". " + coupons[i] + "\n";
+                }
+
+                return rtn == "" ? "Machine is empty" : rtn;
+            }
+            else
+            {
+                return "Machine is empty.";
+            }
+        }
+    }
+}
---
+++ WpfZad1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfZad1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal LotteryMachine Machine;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Machine = new LotteryMachine();
+        }
+
+        private void AddCoupon_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(InputCoupon.Text))
+            {
+                Machine.AddNewCoupon(InputCoupon.Text);
+                InputCoupon.Text = "";
+
+                ShowResult.Content = "Successfully added a new coupon.\n\n" +
+                    "Current machine tickets: \n" + Machine.GetAllCoupons();
+
+            }
+            else
+            {
+                MessageBox.Show("Input field cannot be empty.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GetCoupon_Click(object sender, RoutedEventArgs e)
+        {
+            if(Machine.CouponsAvailable())
+            {
+                string randomCoupon = Machine.GetRandomCoupon();
+
+                ShowResult.Content = "Sucessfully got random coupon.\n\n" +
+                    "Random coupon: \n" + randomCoupon + "\n\n" + 
+                    "Current machine tickets: \n" + Machine.GetAllCoupons();
+            }
+            else
+            {
+                ShowResult.Content = "There aren't any coupons in the machine.\n\n" +
+                    "Current machine tickets: \n" + Machine.GetAllCoupons();
+            }
+        }
+    }
+}
